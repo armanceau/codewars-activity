a18f658f25eb5af5c15e39cb33e5dcf3514b1831
Arthur Manceau <102350268+armanceau@users.noreply.github.com>
2025-04-02 09:24:09 +0200

    Update server.js

--- api/server.js
+++ api/server.js
@@ -79,7 +79,7 @@ function generateSVG(activityData) {
   allDates.forEach((date, index) => {
     const count = activityData[date] || 0;
     if (count <= 0) {
-      fillColor = "#101414";
+      fillColor = "#151b23";
     } else {
       const level =
         dayLevels.find((level) => count <= level.threshold) ||
